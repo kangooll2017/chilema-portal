361e21c744872b13282e2ed17ec30713f851351b
kangooll <328801750@qq.com>
2017-08-08 20:46:20 +0800

    框架搭设完成，初次提交

---
+++ src/main/java/com/chilema/portal/listener/MyAppListener.java
@@ -0,0 +1,24 @@
+package com.chilema.portal.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * 项目启动的时候，将一些常用数据放在application域中
+ */
+public class MyAppListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext servletContext = sce.getServletContext();
+        //1、将项目路径放在application域中
+        servletContext.setAttribute("ctp", servletContext.getContextPath());
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        //2、项目关闭，销毁application域中所有数据
+    }
+
+}
